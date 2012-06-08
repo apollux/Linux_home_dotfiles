4f6d6d4a55cdcad9e0f539a6ce5a91bdf67a2b8e
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2012-06-08 16:30:25 +0200

    made installation more generic

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp bashrc ~/.bashrc
-cp bashrc.d ~/.bashrc.d -r
+for f in `ls ${DIR}/dotfiles`
+do
+    ln -s ${DIR}/dotfiles/$f ~/.$f
+done
