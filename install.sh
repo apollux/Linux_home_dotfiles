3c820feb48a37d6cb2569db5880a9d3df844904f
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2012-06-11 10:40:43 +0200

    added gitconfig

--- install.sh
+++ install.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
+echo "If any of the following files or directories exists in"\
+    "your home directory, they will be removed!"
 for f in `ls ${DIR}/dotfiles`
 do
+    echo ".$f"
+done
+
+read -p "Are you sure you want to continue? " -n 1
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Aborted"
+    exit 1
+fi
+
+for f in `ls ${DIR}/dotfiles`
+do
+    if [ -e ~/.$f ]; then
+        rm ~/.$f -r
+    fi
     ln -s ${DIR}/dotfiles/$f ~/.$f
 done
+echo "Done"
